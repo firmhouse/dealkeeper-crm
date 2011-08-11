6f300c9ae955ffbc66c0ff1ce8b6ea323c76b3d9
Michiel Sikkes <michiel.sikkes@gmail.com>
2011-08-11 11:58:20 +0200

    initial commit for dealkeeper-crm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealkeeperCrm::Application.config.secret_token = 'd5909173d7e98e1eff2f1bbd67860c43594af5603c69dab27bd9b743cb360f96d7cc2a817c3028c04c5a4ea270bab88c69cde4ae3c5d2c24f8ba0d5eb31f0295'
